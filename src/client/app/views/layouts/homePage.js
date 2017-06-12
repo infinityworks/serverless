70a2c01647a6f07edf9f861e6f7b0620bf65446c
Denis Craig <denis.craig@infinityworks.com>
2017-06-12 01:57:24 +0100

    Fixed the link to Tamas' twitter.

--- src/client/app/views/layouts/homePage.js
+++ src/client/app/views/layouts/homePage.js
@@ -10,8 +10,8 @@ export class HomePage extends Component {
           <div className="ruler--hr"></div>
           <p>
             Thanks for listening to our talk at the BCS event. We hope you enjoyed it!
-            This is an example application running on AWS leveraging Serverless technologies.
           </p>
+          <p>This is an example application running on AWS leveraging Serverless technologies.</p>
           <p>
             If you could take the time to fill out the survey on this page, we'll post some metrics on our twitter <a href="https://twitter.com/infinityworks">@InfinityWorks</a> later.
             We will even take a screenshot of the billing
