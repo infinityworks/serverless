304a5774c3e04a19ea771f583e1babccc686bc70
Denis Craig <denis.craig@infinityworks.com>
2017-06-12 01:53:09 +0100

    Started making a pretty front-end website.

--- src/client/app/views/layouts/homePage.js
+++ src/client/app/views/layouts/homePage.js
@@ -4,8 +4,28 @@ export class HomePage extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Hello World!</h1>
+      <div className="content">
+        <div className="flush  soft__triple  background--white  brt--half  bb--grey">
+          <h1 className="flush">Serverless Architecture</h1>
+          <div className="ruler--hr"></div>
+          <p>
+            Thanks for listening to our talk at the BCS event. We hope you enjoyed it!
+            This is an example application running on AWS leveraging Serverless technologies.
+          </p>
+          <p>
+            If you could take the time to fill out the survey on this page, we'll post some metrics on our twitter <a href="https://twitter.com/infinityworks">@InfinityWorks</a> later.
+            We will even take a screenshot of the billing
+            page as proof that it costs next to nothing to run. ;)
+          </p>
+        </div>
+        <div className="flush  soft__triple  background--white  brb--half">
+          <h2 className="flush">The Survey</h2>
+          <div className="ruler--hr"></div>
+          <form>
+            <input type="text" placeholder="Name"></input>
+            <input type="text" placeholder="Email Address"></input>
+          </form>
+        </div>
       </div>
     );
   }
